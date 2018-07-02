ec6b5e2f562fd42ed28a29e4bd52a251cd9d8c90
zkp <206127066@qq.com>
2018-07-02 17:14:39 +0800

    第一次提交项目基础结构和基础代码

---
+++ app-dao/src/main/java/com/baizhi/dao/HelloDao.java
@@ -0,0 +1,14 @@
+package com.baizhi.dao;
+
+/**
+ * Created by Administrator on 2018/7/1 0001.
+ */
+public class HelloDao {
+
+    public String sayHello(String name){
+
+        return "name:"+name;
+
+    }
+
+}
